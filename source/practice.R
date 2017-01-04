1d6c32ebec2b1b0a2ab3ef1bf05699597dd126df
Sargsyan <dsargsy@its.jnj.com>
2017-01-04 15:32:10 -0500

    Some changes

--- source/practice.R
+++ source/practice.R
@@ -1,1 +1,14 @@
-x=1
+# Project: Tom's Practice Project
+# Author: Tom Kelly
+# Created: 01/04/2017
+#**********************************
+# Load packages----
+require(data.table)
+
+# Create dummy data----
+dt1 <- data.table(x = rep(1:100, 100),
+                  y = rnorm(10000))
+dt1
+
+# Plot
+plot(dt1)
